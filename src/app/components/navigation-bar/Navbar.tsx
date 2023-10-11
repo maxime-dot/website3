ec17d503eefc499fc260357bac2f7ab61835d80b
nalahatra-akata <fitiavananalahatra@gmail.com>
2023-10-11 15:13:48 +0300

    feat: set navigation link as object

--- src/app/components/navigation-bar/Navbar.tsx
+++ src/app/components/navigation-bar/Navbar.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import Link from "next/link";
 import "./navbar.scss";
+import Button from "../button/Button";
 const Navbar: React.FC = () => {
+  const Links: { name: string; path: string }[] = [
+    { name: "Home", path: "#" },
+    { name: "Services", path: "#services" },
+    { name: "Teams", path: "#teams" },
+    { name: "Projects", path: "#projects" },
+    { name: "Articles", path: "#articles" },
+    { name: "Contacts", path: "#contacts" },
+  ];
+
   return (
     <nav className="akata-navbar">
-      <p>Navbar components is now working</p>
+      <div className="container navbar-content">
+        <p>navbrand</p>
+        <div className="navlink">
+          {Links &&
+            Links.length > 0 &&
+            Links.map(({ name, path }, index) => (
+              <Link key={index} href={path} title={`Akata ${name}`}>
+                {name}
+              </Link>
+            ))}
+        </div>
+        <Button title="LET'S TALK" hoverType="solid" />
+      </div>
     </nav>
   );
 };
