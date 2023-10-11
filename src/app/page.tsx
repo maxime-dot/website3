f342a6d70b352946cda20a7653176a4e96477f50
nalahatra-akata <fitiavananalahatra@gmail.com>
2023-10-11 14:20:35 +0300

    refactor<NextJS>: remove CI/CD Test components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
 import styles from "./page.module.scss";
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Image
-        width={1052.75 / 1.4}
-        height={735.52 / 1.4}
-        src={"/Maskoty.png"}
-        alt="akata-maki-representation"
-        className="maki"
-      />
-      <p>See you soon....</p>
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
