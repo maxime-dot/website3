64d354a0343812d1cf470f8a61d6c11b1499298c
nalahatra-akata <fitiavananalahatra@gmail.com>
2023-10-11 14:38:49 +0300

    feat: setting up navbar function

---
+++ src/app/components/navigation-bar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navbar.scss";
+const Navbar: React.FC = () => {
+  return (
+    <nav className="akata-navbar">
+      <p>Navbar components is now working</p>
+    </nav>
+  );
+};
+
+export default Navbar;
