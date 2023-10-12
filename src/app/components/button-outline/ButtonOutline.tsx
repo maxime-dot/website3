87ead4b7f7cc20b6035c1b799736dc2a5cb53439
nalahatra-akata <fitiavananalahatra@gmail.com>
2023-10-12 20:24:23 +0300

    refactor(ButtonOutline): add function a as props to handle click event

--- src/app/components/button-outline/ButtonOutline.tsx
+++ src/app/components/button-outline/ButtonOutline.tsx
@@ -3,10 +3,15 @@ import "./button-outline.scss";
 
 interface ButtonProps {
   title: string;
+  onClick: () => void;
 }
 
-const ButtonOutline: React.FC<ButtonProps> = ({ title }) => {
-  return <button className="akata-button-outline">{title}</button>;
+const ButtonOutline: React.FC<ButtonProps> = ({ title, onClick }) => {
+  return (
+    <button className="akata-button-outline" onClick={onClick}>
+      {title}
+    </button>
+  );
 };
 
 export default ButtonOutline;
