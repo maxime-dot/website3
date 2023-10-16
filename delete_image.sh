d5e9f1a63078dd661ce7d3fb388b56cd7974ba75
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-10-16 08:01:27 +0000

    cicd: add condition in the script delete_image

--- delete_image.sh
+++ delete_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-docker images | grep "cicd" | tr -s " " | cut -d " " -f3 | xargs docker rmi
-docker images | grep "none" | tr -s " " | cut -d " " -f3 | xargs docker rmi
+docker images | grep "cicd" | tr -s " " | cut -d " " -f3 | xargs docker rmi || true
+docker images | grep "none" | tr -s " " | cut -d " " -f3 | xargs docker rmi || true
