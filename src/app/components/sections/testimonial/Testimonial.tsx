59a207a816c772c17776010db28d24549e09f6af
nalahatra-akata <fitiavananalahatra@gmail.com>
2023-10-18 10:25:29 +0300

    feat: setting up testimonial section components

---
+++ src/app/components/sections/testimonial/Testimonial.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./testimonial.scss";
+const Testimonial: React.FC = () => {
+  return (
+    <section
+      className="akata-testimonial fill-view  container"
+      id="#testimonal"
+    >
+      testimonial
+    </section>
+  );
+};
+
+export default Testimonial;
