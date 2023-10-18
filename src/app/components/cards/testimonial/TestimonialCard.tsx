70a129d36a66902f241578e872c577688366c26d
nalahatra-akata <fitiavananalahatra@gmail.com>
2023-10-18 16:57:30 +0300

    refactor: change customer testimonial card as props

--- src/app/components/cards/testimonial/TestimonialCard.tsx
+++ src/app/components/cards/testimonial/TestimonialCard.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import "./testimonial-card.scss";
-const TestimonialCard: React.FC = () => {
+import Image from "next/image";
+interface CardProps {
+  name: string;
+  profile: string;
+  testimonials: string;
+  post: string;
+}
+const TestimonialCard: React.FC<CardProps> = ({
+  name,
+  profile,
+  post,
+  testimonials,
+}) => {
   return (
     <div className="testimonial-card d-flex flex-col">
-      <p className="akata-text-small">
-        « We greatly appreciate having you all with us as well. We all seem to
-        not only work well together which is important, but any of our requests,
-        your group jumps right in without hesitation. Pety has been helping out
-        with some systems stuff and I think has even been learning a little on
-        that end overall for myself, I know I have learned a lot from the group.
-        Beyond client/developer relations, I do enjoy and value the unique
-        partnership we all have formed and think it is great when we can also
-        pick up knowledge along the way 🙂 ».
-      </p>
+      <p className="akata-text-small">{testimonials}</p>
       <div className="customer-info d-flex flex-row">
-        <div className="img"></div>
+        <Image
+          src={profile}
+          width={53}
+          height={53}
+          alt={`${name} profile`}
+          className="img"
+          priority={true}
+        />
         <div className="info-perso d-flex flex-col">
-          <span className="akata-title-medium name">CASEY HARDING</span>
-          <p className="akata-text-small post">
-            Manager of system & people - Far Frontier Studio
-          </p>
+          <span className="akata-title-medium name">{name}</span>
+          <p className="akata-text-small post">{post}</p>
         </div>
       </div>
     </div>
--- src/app/components/sections/testimonial/Testimonial.tsx
+++ src/app/components/sections/testimonial/Testimonial.tsx
@@ -25,7 +25,7 @@ const Testimonial: React.FC = () => {
   };
   return (
     <section
-      className="akata-testimonial fill-view   d-flex flex-row"
+      className="akata-testimonial w-100   d-flex flex-row"
       id="#testimonal"
     >
       <Image
@@ -43,7 +43,7 @@ const Testimonial: React.FC = () => {
             <h2 className="akata-title-strong">
               What our <span>customer</span> say about us?
             </h2>
-            <p className="akata-text-big">
+            <p className="akata-text-big description">
               Experience the love our clients have for our IT agency&apos;s
               work. Explore their glowing testimonials and discover why they
               trust us with their technology needs.
@@ -66,15 +66,7 @@ const Testimonial: React.FC = () => {
           </div>
         </div>
         <div className="testimonial-customer">
-          <div className="customer d-flex flex-col">
-            <TestimonialCard />
-            <TestimonialCard />
-          </div>
-          <div className="customer d-flex flex-col">
-            <TestimonialCard />
-            <TestimonialCard />
-            <TestimonialCard />
-          </div>
+          <TestimonialCard name="casey harding" post="Manager of system & people - Far Frontier Studio" profile="/images/customer/casey-harding.png" testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »." />
         </div>
       </div>
     </section>
