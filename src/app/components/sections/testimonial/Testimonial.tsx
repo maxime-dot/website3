e868e3d4c91ce9a7ea88a423b8fe155984ee08dd
nalahatra-akata <nalahatra-akata@goavana.com>
2023-10-19 10:57:06 +0300

    feat: add card scroll on mobile design

--- src/app/components/sections/testimonial/Testimonial.tsx
+++ src/app/components/sections/testimonial/Testimonial.tsx
@@ -49,6 +49,45 @@ const Testimonial: React.FC = () => {
               trust us with their technology needs.
             </p>
           </div>
+
+          <div className="card-slides">
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« Wees great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+          </div>
           <div className="button-group d-flex flex-row">
             <Button
               hoverType="shadowed"
@@ -66,36 +105,40 @@ const Testimonial: React.FC = () => {
           </div>
         </div>
         <div className="testimonial-customer d-flex">
-          <TestimonialCard
-            name="casey harding"
-            post="Manager of system & people - Far Frontier Studio"
-            profile="/images/customer/casey-harding.png"
-            testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
-          />
-          <TestimonialCard
-            name="casey harding"
-            post="Manager of system & people - Far Frontier Studio"
-            profile="/images/customer/casey-harding.png"
-            testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
-          />
-          <TestimonialCard
-            name="casey harding"
-            post="Manager of system & people - Far Frontier Studio"
-            profile="/images/customer/casey-harding.png"
-            testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
-          />
-          <TestimonialCard
-            name="casey harding"
-            post="Manager of system & people - Far Frontier Studio"
-            profile="/images/customer/casey-harding.png"
-            testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
-          />
-          <TestimonialCard
-            name="casey harding"
-            post="Manager of system & people - Far Frontier Studio"
-            profile="/images/customer/casey-harding.png"
-            testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
-          />
+          <div className="card-container d-flex flex-col">
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="EDOUARD DELGATTE"
+              post="Founder of Indemnflight"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« I’ve been working with Akata and its founder for over two years now to evolve our site indemnflight.com (WordPress/Php) as well as backoffice APIs (nodeJS) that allow us to treat air traffic data. Everything has always gone well. Akata works independently and knows how to make itself available for emergencies. Thanks to the whole team. »"
+            />
+          </div>
+          <div className="card-container d-flex flex-col">
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+            <TestimonialCard
+              name="casey harding"
+              post="Manager of system & people - Far Frontier Studio"
+              profile="/images/customer/casey-harding.png"
+              testimonials="« We greatly appreciate having you all with us as well. We all seem to not only work well together which is important, but any of our requests, your group jumps right in without hesitation. Pety has been helping out with some systems stuff and I think has even been learning a little on that end overall for myself, I know I have learned a lot from the group. Beyond client/developer relations, I do enjoy and value the unique partnership we all have formed and think it is great when we can also pick up knowledge along the way 🙂 »."
+            />
+          </div>
         </div>
       </div>
     </section>
