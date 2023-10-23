9676ec4777a63c0104794866b25dc816538f122d
nalahatra-akata <nalahatra-akata@goavana.com>
2023-10-23 17:31:50 +0300

    fix: remove second scroll bar on component open

--- src/app/components/navigation-bar/Navbar.tsx
+++ src/app/components/navigation-bar/Navbar.tsx
@@ -108,4 +108,4 @@ const Navbar: React.FC = () => {
   );
 };
 
-export default Navbar;
+export default React.memo(Navbar);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+import { useState } from "react";
 import Header from "./components/sections/header/Header";
 import Navbar from "./components/navigation-bar/Navbar";
 import styles from "./page.module.scss";
 import Testimonial from "./components/sections/testimonial/Testimonial";
 
 export default function Home() {
+  const [overflowHidden, setOverflowHidden] = useState(false);
+
+  // Function to update the overflow state
+
   return (
-    <main className={styles.main}>
+    <main
+      className={styles.main}
+      style={{ overflow: overflowHidden ? "hidden" : "auto" }}
+    >
       <Navbar />
       <Header />
-      <Testimonial />
+      <Testimonial onOpenSlider={() => setOverflowHidden(!overflowHidden)} />
     </main>
   );
 }
