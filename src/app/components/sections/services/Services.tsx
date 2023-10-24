2b84514c2741f325bdd351ada8538d1c2415ec1e
nalahatra-akata <nalahatra-akata@goavana.com>
2023-10-24 09:41:37 +0300

    feat: setting up services page boilerplate

---
+++ src/app/components/sections/services/Services.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./services.scss";
+const Services: React.FC = () => {
+  return (
+    <section className="services fill-view" id="services">
+      <p>the services page is working</p>
+    </section>
+  );
+};
+
+export default Services;
