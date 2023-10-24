ea9c8e856d58995ac78f4c29c8fad6c077c2b736
nalahatra-akata <nalahatra-akata@goavana.com>
2023-10-24 10:49:13 +0300

    feat: setting up not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,34 @@
+"use  client";
 import React from "react";
 import Image from "next/image";
+import style from "./not-found.module.scss";
+
 const NotFound: React.FC = () => {
+  const handleBackHome = () => {
+    return;
+  };
   return (
-    <div className="not-found-page container fill-view d-flex-center">
+    <div className={style.notFound}>
       <Image
-        src={"/images/not-found-hero.png"}
+        src="/images/not-found-hero.png"
         alt="Image hero."
         width={997.62}
         height={718.46}
+        className={style.hero}
       />
-      <div className="not-fount-info"></div>
+      <div className={style.notFoundInfo}>
+        <div className={style.infoText}>
+          <h1 className={style.title}>
+            <span>Oupps!</span> 404 Not Found
+            <div className={style.titleBars} />
+          </h1>
+          <p className="akata-text-big">
+            Sincerely sorry, but it seems that the page you were looking for is
+            either not found or unavailable. This could be due to an internet
+            connection problem or incorrect page navigation.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
