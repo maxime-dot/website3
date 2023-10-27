b1da56f45b186a762a2af0235164568fda732f18
nalahatra-akata <nalahatra-akata@goavana.com>
2023-10-27 11:37:48 +0300

    feat: add info on teams

--- src/app/components/sections/teams/Teams.tsx
+++ src/app/components/sections/teams/Teams.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 import "./teams.scss";
+import Button from "../../button/normal/Button";
+import ButtonOutline from "../../button/outline/ButtonOutline";
+
 const Teams: React.FC = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => {
+    setOpenModal(true)
+  }
   return (
     <section className="akata-teams fill-view  container" id="teams">
       <div className="teams-intro d-flex flex-col">
@@ -15,6 +26,26 @@ const Teams: React.FC = () => {
             solutions that drive your success. Explore the faces behind our
             innovation.
           </p>
+          <div className="profile-social-link d-flex flex-row">
+            <Link
+              href={"https://www.facebook.com"}
+              passHref={true}
+              className="social-link"
+            >
+              <FontAwesomeIcon icon={faHome} className="link-icon" />
+            </Link>
+            <Link
+              href={"https://www.facebook.com"}
+              passHref={true}
+              className="social-link"
+            >
+              <FontAwesomeIcon icon={faHome} className="link-icon" />
+            </Link>
+          </div>
+        </div>
+        <div className="intro-ca-button d-flex flex-row">
+          <Button type="button" ariaLabel="send you project requirement" content="LET'S TALK" onClick={handleOpenModal} hoverType="shadowed"  />
+          <ButtonOutline ariaLabel="Get better to know you teams" content="ABOUT US" onClick={handleOpenModal} title="Get Better to know your teams" />
         </div>
       </div>
       <div className="teams-admin-member"></div>
