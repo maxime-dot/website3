33862ef838a5ba3512cd531f0daa3e84635900fd
nalahatra-akata <nalahatra-akata@goavana.com>
2023-10-31 09:54:40 +0300

    refactor: change card ui style +  text trucate function

---
+++ src/app/components/explore-articles/ExploreArticles.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./expore-article.scss";
+const ExploreArticles: React.FC = () => {
+  return (
+    <div className="akata-explore-acticles fill-view">
+      <p>arciles explore is working....</p>
+    </div>
+  );
+};
+
+export default ExploreArticles;
