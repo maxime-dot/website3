23fc8bd477d9a92fef68726d335d8dd0a53b71a4
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-02 15:25:39 +0300

    feat: setting up team page boiler plate

---
+++ src/app/teams/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./team-page.scss";
+export const TeamsPage: React.FC = () => {
+  return <div className="akata-team-page">Teams page is working...</div>;
+};
+export default TeamsPage;
