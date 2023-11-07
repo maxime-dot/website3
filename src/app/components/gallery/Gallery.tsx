5f64f701c811bcd34f1d42e8f2bef017cf4a6e11
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-07 11:36:46 +0300

    feat: add team gallery inside json data

--- src/app/components/gallery/Gallery.tsx
+++ src/app/components/gallery/Gallery.tsx
@@ -4,6 +4,7 @@ import {motion, AnimatePresence} from "framer-motion"
 import Image from "next/image";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleXmark} from "@fortawesome/free-solid-svg-icons";
+import GalleryData from "@/app/data/team-gallery.json";
 
 interface GalleryProps {
     onClose?: () => void;
@@ -29,30 +30,21 @@ const Gallery: React.FC<GalleryProps> = ({onClose}) => {
                     </AnimatePresence>
                 </div>
                 <div className="picture-list">
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
-                    <Image src={"/images/teams/akata-team-5.png"} alt={"akata teams"}
-                           width={800} height={543} className={"list-thumb"}
-                    />
+                    {GalleryData && GalleryData.length > 0 && GalleryData.map((data, index) => (
+                        <motion.div key={data.id}
+                                    viewport={{once: true}}
+                                    whileInView={{opacity: 1, y: 0}}
+                                    initial={{opacity: 0, y: 10}}
+                                    transition={{duration: .3, delay: .3}}>
+                            <Image
+                                src={data.imgSrc}
+                                alt={data.alt}
+                                width={800}
+                                height={543}
+                                className={"list-thumb"}
+                            />
+                        </motion.div>
+                    ))}
                 </div>
             </motion.div>
         </div>
