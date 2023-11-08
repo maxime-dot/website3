bb823834e5551fbae563dfbceb5b443cda2cf5e9
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-08 11:56:31 +0300

    feat: custome close button ui

--- src/app/components/sliders/team-slider/TeamSlider.tsx
+++ src/app/components/sliders/team-slider/TeamSlider.tsx
@@ -1,7 +1,11 @@
 "use client";
 import React from "react"
 import {motion} from "framer-motion"
+
 import "./team-slider.scss"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faXmark} from "@fortawesome/free-solid-svg-icons";
+
 
 interface SliderProps {
     onClose: () => void
@@ -10,7 +14,11 @@ interface SliderProps {
 const TeamSlider: React.FC<SliderProps> = ({onClose}) => {
     return (
         <motion.div initial={{opacity: 0, y: 80}} animate={{opacity: 1, y: 0}} className="akata-team-slider fill-view">
-            <button onClick={onClose}>close</button>
+            <div className="main-content">
+                <button onClick={onClose} className={"btn-close-slider"}>
+                    <FontAwesomeIcon icon={faXmark}
+                    /></button>
+            </div>
         </motion.div>
     )
 }
