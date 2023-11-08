ba7a7cd14f7c2a8f91c3b0db80cf5129a28d2b59
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-08 14:17:03 +0300

    feat(TeamSlider): add profile info

--- src/app/components/sliders/team-slider/TeamSlider.tsx
+++ src/app/components/sliders/team-slider/TeamSlider.tsx
@@ -15,6 +15,10 @@ const TeamSlider: React.FC<SliderProps> = ({onClose}) => {
     return (
         <motion.div initial={{opacity: 0, y: 80}} animate={{opacity: 1, y: 0}} className="akata-team-slider fill-view">
             <div className="main-content">
+                <div className="slider-container container d-flex-space-between">
+                    <strong className="akata-title-medium profile-name">RAMIANDRISOA ANDRIAMANDRATO PACKTRICK
+                        (BODGE)</strong>
+                </div>
                 <button onClick={onClose} className={"btn-close-slider"}>
                     <FontAwesomeIcon icon={faXmark}/>
                 </button>
