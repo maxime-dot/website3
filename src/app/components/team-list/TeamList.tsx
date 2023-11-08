e4b9ac3c41a20a7033471b5f2ed20542e358e535
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-08 06:47:56 +0300

    feat: add data on team-list

--- src/app/components/team-list/TeamList.tsx
+++ src/app/components/team-list/TeamList.tsx
@@ -1,18 +1,62 @@
 "use client";
 import React from "react";
 import "./team-list.scss"
-import { motion } from "framer-motion"
+import {motion} from "framer-motion"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faLinkedin, faGithub, faDribbble} from "@fortawesome/free-brands-svg-icons";
+import TeamData from "@/app/data/team-list.json"
+import Link from "next/link";
+
 const TeamList: React.FC = () => {
-    return(
+    return (
         <div className="akata-team-list d-flex-center flex-col">
-            <motion.h2 initial={{opacity: 0, y: 20}}   whileInView={{opacity: 1, y: 0}} transition={{delay: .3}} viewport={{once: true}} className="akata-title-medium">Meet our exceptional <span>team</span></motion.h2>
-            <motion.p  initial={{opacity: 0, y: 20}}   whileInView={{opacity: 1, y: 0}} transition={{delay: .3}} viewport={{once: true}} className="akata-text-medium team-title-description">Meet the exceptional clients who have chosen Akata Goavana for their IT needs. Our successful partnerships with these esteemed organizations showcase our dedication to excellence and innovation. Join our ever-expanding client family and let us lead you toward digital success</motion.p>
+            <motion.h2 initial={{opacity: 0, y: 20}} whileInView={{opacity: 1, y: 0}} transition={{delay: .3}}
+                       viewport={{once: true}} className="akata-title-medium">Meet our exceptional <span>team</span>
+            </motion.h2>
+            <motion.p initial={{opacity: 0, y: 20}} whileInView={{opacity: 1, y: 0}} transition={{delay: .3}}
+                      viewport={{once: true}} className="akata-text-medium team-title-description">Meet the exceptional
+                clients who have chosen Akata Goavana for their IT needs. Our successful partnerships with these
+                esteemed organizations showcase our dedication to excellence and innovation. Join our ever-expanding
+                client family and let us lead you toward digital success
+            </motion.p>
             <div className="list-content container">
-                <div className="team-item"></div>
-                <div className="team-item"></div>
-                <div className="team-item"></div>
-                <div className="team-item"></div>
-                <div className="team-item"></div>
+
+                {TeamData.map((data, index) => (
+                    <div className="team-item d-flex-space-between flex-col" key={`team-${index}`}>
+                        <div className="profile-pics"></div>
+                        <div className="profile-info d-flex flex-col">
+                            <span className={"profile-name"}>{data.firstName}  {data.lastName}</span>
+                            <span className={"profile-post"}>{data.post}</span>
+                        </div>
+                        <div className="profile-social-link d-flex flex-row">
+                            {data.socialLink.facebook &&
+                                <Link href={data.socialLink.facebook} passHref={true} target={"_blank"}
+                                      className={"link-item"}>
+                                    <FontAwesomeIcon icon={faFacebook}/>
+                                </Link>}
+                            {data.socialLink.linkedin &&
+                                <Link href={data.socialLink.linkedin} passHref={true} target={"_blank"}
+                                      className={"link-item"}>
+                                    <FontAwesomeIcon icon={faLinkedin}/>
+                                </Link>}
+                            {
+                                data.socialLink.github &&
+                                <Link href={data.socialLink.github} passHref={true} target={"_blank"}
+                                      className={"link-item"}>
+                                    <FontAwesomeIcon icon={faGithub}/>
+                                </Link>
+                            }
+                            {
+                                data.socialLink.dribble &&
+                                <Link href={data.socialLink.dribble} passHref={true} target={"_blank"}
+                                      className={"link-item"}>
+                                    <FontAwesomeIcon icon={faDribbble}/>
+                                </Link>
+                            }
+                        </div>
+                    </div>
+                ))}
+
             </div>
         </div>
     )
