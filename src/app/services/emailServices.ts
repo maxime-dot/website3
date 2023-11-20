72aa9d4a416706e19ee4085454d6f9679fad65ae
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-20 17:41:13 +0300

    reafactor: format code for better readabilty

--- src/app/services/emailServices.ts
+++ src/app/services/emailServices.ts
@@ -2,24 +2,23 @@ import nodemailer from "nodemailer";
 import {writeFile} from "fs/promises";
 import {File} from "buffer";
 
-
 const mail = process.env.MAIL_USER;
 const pass = process.env.MAIL_PASSWORD;
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: mail,
-        pass: pass,
-    },
+  service: "gmail",
+  auth: {
+    user: mail,
+    pass: pass,
+  },
 });
 
 export async function sendEmail({EMAIL, SUBJECT, MESSAGE, NAME}: any) {
-    const mailOptions = {
-        from: mail,
-        to: mail,
-        subject: `AKATA-WEBSITE |  ${SUBJECT} from ${NAME}`,
-        html: `
+  const mailOptions = {
+    from: mail,
+    to: mail,
+    subject: `AKATA-WEBSITE |  ${SUBJECT} from ${NAME}`,
+    html: `
       <div>
         <p>
           Hi, this is an email from <b> ${NAME} </b> - <i>${EMAIL}</i><br> <br>
@@ -31,63 +30,63 @@ export async function sendEmail({EMAIL, SUBJECT, MESSAGE, NAME}: any) {
         </p>
         <small>Akata Goavana, Website 3.0</small>
       </div>`,
-    };
+  };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        return {success: true};
-    } catch (error) {
-        return {success: false, error};
-    }
+  try {
+    await transporter.sendMail(mailOptions);
+    return {success: true};
+  } catch (error) {
+    return {success: false, error};
+  }
 }
 
 export async function sendEmailWithAttachments({
-                                                   NAME,
-                                                   EMAIL,
-                                                   SERVICE,
-                                                   files,
-                                               }: {
-    NAME: string;
-    EMAIL: string;
-    SERVICE: string;
-    files: File[];
+  NAME,
+  EMAIL,
+  SERVICE,
+  files,
+}: {
+  NAME: string;
+  EMAIL: string;
+  SERVICE: string;
+  files: File[];
 }) {
-    const attachments = [];
+  const attachments = [];
 
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const bytes = await file.arrayBuffer();
-        const buffer = Buffer.from(bytes);
-        const path = `./src/upload/${EMAIL}-${file.name}`;
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    const path = `./src/upload/${EMAIL}-${file.name}`;
 
-        try {
-            await writeFile(path, buffer);
-            attachments.push({
-                filename: file.name,
-                path: path,
-            });
-        } catch (error) {
-            throw new Error("Error saving file");
-        }
+    try {
+      await writeFile(path, buffer);
+      attachments.push({
+        filename: file.name,
+        path: path,
+      });
+    } catch (error) {
+      throw new Error("Error saving file");
     }
+  }
 
-    const mailOptions = {
-        from: mail,
-        to: mail,
-        subject: `AKATA-WEBSITE | ${SERVICE} project requirement from ${NAME}`,
-        html: `
+  const mailOptions = {
+    from: mail,
+    to: mail,
+    subject: `AKATA-WEBSITE | ${SERVICE} project requirement from ${NAME}`,
+    html: `
       <div>
         <p> 
           Hi, this is an email from <b>${NAME}</b> - <i>${EMAIL}</i>, who wants to talk about <b>${SERVICE}</b>                
         </p>            
       </div>`,
-        attachments: attachments,
-    };
+    attachments: attachments,
+  };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        return {success: true};
-    } catch (error) {
-        throw new Error("Error sending email");
-    }
+  try {
+    await transporter.sendMail(mailOptions);
+    return {success: true};
+  } catch (error) {
+    throw new Error("Error sending email");
+  }
 }
