b225372f9e04ae063c7b4362b56babeeedb8a7d6
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-27 16:53:09 +0300

    refactor: format + change headlines title

--- src/app/components/sections/header/Header.tsx
+++ src/app/components/sections/header/Header.tsx
@@ -7,82 +7,97 @@ import "./header.scss";
 
 const Button = dynamic(() => import("../../button/normal/Button"));
 const ModalLetsTalk = dynamic(
-    () => import("../../modal-lets-talk/ModalLetsTalk"),
-    {
-        loading: () => <LoadingModal/>,
-    }
+  () => import("../../modal-lets-talk/ModalLetsTalk"),
+  {
+    loading: () => <LoadingModal />,
+  }
 );
-import {motion} from "framer-motion"
+import {motion} from "framer-motion";
 import ButtonOutlineIconified from "../../button/outline-iconified/ButtonOutlineIconified";
 import ButtonCaFloat from "../../button/ca-float/ButtonCaFloat";
 import LoadingModal from "../../modal-lets-talk/LoadingModal";
 
 const VideoPlayer = dynamic(() => import("../../video-player/VideoPlayer"), {
-    loading: () => <LoadingModal/>,
+  loading: () => <LoadingModal />,
 });
 const Header: React.FC = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [playVideo, setPlayVideo] = useState(false);
-    return (
-        <header
-            className="akata-header fill-view d-flex-space-between container"
-            id="home"
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [playVideo, setPlayVideo] = useState(false);
+  return (
+    <header
+      className="akata-header fill-view d-flex-space-between container"
+      id="home"
+    >
+      {playVideo && (
+        <VideoPlayer
+          id="4jcOdfQxVY0"
+          title="AKATA GOAVANA"
+          onClose={() => setPlayVideo(false)}
+        />
+      )}
+      <ButtonCaFloat
+        FontAwesome={faRocket}
+        ariaLabel="Submit project requirement"
+        onClick={() => setIsModalOpen(true)}
+      />
+      {isModalOpen && <ModalLetsTalk onClose={() => setIsModalOpen(false)} />}
+      <div className="header-content d-flex flex-col w-100">
+        <div className="content-text d-flex flex-col">
+          <motion.h1
+            initial={{opacity: 0, y: 30}}
+            animate={{opacity: 1, y: 0}}
+            className="akata-title-strong"
+          >
+            THE <span>CASH COW</span> OF YOUR BUSINESS
+          </motion.h1>
+          <motion.p
+            initial={{opacity: 0, y: 30}}
+            animate={{opacity: 1, y: 0}}
+            transition={{delay: 0.1}}
+            className="akata-text-big"
+          >
+            At Akata Goavana, we are dedicated to the well-being of your IT
+            infrastructure. Our experts are here to provide exceptional care for
+            your systems. Trust in our expertise to keep them running smoothly.
+          </motion.p>
+        </div>
+        <motion.div
+          initial={{opacity: 0, y: 30}}
+          animate={{opacity: 1, y: 0}}
+          transition={{delay: 0.2}}
+          className="content-ca-button d-flex flex-row"
         >
-            {playVideo && (
-                <VideoPlayer
-                    id="4jcOdfQxVY0"
-                    title="AKATA GOAVANA"
-                    onClose={() => setPlayVideo(false)}
-                />
-            )}
-            <ButtonCaFloat
-                FontAwesome={faRocket}
-                ariaLabel="Submit project requirement"
-                onClick={() => setIsModalOpen(true)}
-            />
-            {isModalOpen && <ModalLetsTalk onClose={() => setIsModalOpen(false)}/>}
-            <div className="header-content d-flex flex-col w-100">
-                <div className="content-text d-flex flex-col">
-                    <motion.h1 initial={{opacity: 0, y: 30}} animate={{opacity: 1, y: 0}}
-                               className="akata-title-strong">
-                        lET’S US TURN YOUR <span>DREAM</span> INTO REALITY
-                    </motion.h1>
-                    <motion.p initial={{opacity: 0, y: 30}} animate={{opacity: 1, y: 0}} transition={{delay: .1}}
-                              className="akata-text-big">
-                        At Akata Goavana, we are dedicated to the well-being of your IT
-                        infrastructure. Our experts are here to provide exceptional care for
-                        your systems. Trust in our expertise to keep them running smoothly.
-                    </motion.p>
-                </div>
-                <motion.div initial={{opacity: 0, y: 30}} animate={{opacity: 1, y: 0}} transition={{delay: .2}}
-                            className="content-ca-button d-flex flex-row">
-                    <Button
-                        type="button"
-                        content="LET'S TALK"
-                        hoverType="shadowed"
-                        onClick={() => setIsModalOpen(true)}
-                        ariaLabel="Open let's talk modal to submit your project requirement"
-                    />
-                    <ButtonOutlineIconified
-                        title="Get to Know Us Better"
-                        name="Play akata's video"
-                        onClick={() => setPlayVideo(true)}
-                    />
-                </motion.div>
-            </div>
-            <motion.div initial={{opacity: 0, y: 30}} animate={{opacity: 1, y: 0}} transition={{delay: .2}}
-                        className="header-hero d-flex-center w-100">
-                <Image
-                    src={"/images/maki.webp"}
-                    alt="Akata goavana, hero image"
-                    className="hero-image"
-                    priority={true}
-                    width={600}
-                    height={600}
-                />
-            </motion.div>
-        </header>
-    );
+          <Button
+            type="button"
+            content="LET'S TALK"
+            hoverType="shadowed"
+            onClick={() => setIsModalOpen(true)}
+            ariaLabel="Open let's talk modal to submit your project requirement"
+          />
+          <ButtonOutlineIconified
+            title="Get to Know Us Better"
+            name="Play akata's video"
+            onClick={() => setPlayVideo(true)}
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{opacity: 0, y: 30}}
+        animate={{opacity: 1, y: 0}}
+        transition={{delay: 0.2}}
+        className="header-hero d-flex-center w-100"
+      >
+        <Image
+          src={"/images/maki.webp"}
+          alt="Akata goavana, hero image"
+          className="hero-image"
+          priority={true}
+          width={600}
+          height={600}
+        />
+      </motion.div>
+    </header>
+  );
 };
 
 export default React.memo(Header);
