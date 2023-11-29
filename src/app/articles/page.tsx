04bfcbffb337f465d55e37487eec13903163cfd8
nalahatra-akata <nalahatra-akata@goavana.com>
2023-11-29 12:14:36 +0300

    refactor:  add footer into article page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -11,6 +11,7 @@ import Button from "@/components/button/normal/Button";
 import ArticleData from "@/data/articles.json";
 import ArticleCard from "@/components/cards/articles/ArticleCard";
 import {truncate} from "@/helpers/truncate";
+import Footer from "@/components/footer/Footer";
 const ModalLetsTalk = dynamic(
   () => import("@/components/modal-lets-talk/ModalLetsTalk"),
 
@@ -27,81 +28,84 @@ export default function Page() {
     setOpenModal(true);
   };
   return (
-    <div className="akata-articles-page container">
-      {isOpenModal && <ModalLetsTalk onClose={closeModal} />}
-      <Link
-        href={"/#articles"}
-        className="return-link"
-        as="/#articles"
-        title="Go back to home page"
-      >
-        <FontAwesomeIcon icon={faArrowLeft} className="link-icon" />
-        Go back
-      </Link>
-      <header className="akata-article-header w-100">
-        <div className="header-title d-flex flex-col">
-          <div className="title-text d-flex flex-col">
-            <h1 className="akata-title-strong">
-              Explore the <span> World of Innovation</span>
-            </h1>
-            <p className="content akata-text-big">
-              Dive into our captivating articles to stay at the forefront of
-              technology and innovation. Explore exciting topics and in-depth
-              analyses that will inspire you to push boundaries. Join us on our
-              quest for knowledge and inspiration
-            </p>
-          </div>
-          <Button
-            ariaLabel="Let's talk about your project requirement"
-            content="LET'S TALK"
-            onClick={openModal}
-            type="button"
-            hoverType="shadowed"
-          />
-        </div>
-        <div className="header-article-suggestion d-flex-space-between flex-col">
-          {ArticleData.slice(0, 2).map((data, index) => (
-            <div
-              className="horizontal-article d-flex "
-              key={`horizontal-article-${index}`}
-            >
-              <button className="read-more btn">
-                <FontAwesomeIcon icon={faChevronRight} className="btn-icon" />
-              </button>
-              <Image
-                className="article-poster"
-                alt="article poster"
-                src={data.imageSrc}
-                width={220}
-                height={166}
-              />
-              <div className="article-text-content d-flex flex-col">
-                <span className="date">{data.date}</span>
-                <h3 className="title">{data.title}</h3>
-                <p className="content akata-text-small">
-                  {truncate(data.content, 127)}
-                </p>
-              </div>
+    <>
+      <div className="akata-articles-page container">
+        {isOpenModal && <ModalLetsTalk onClose={closeModal} />}
+        <Link
+          href={"/#articles"}
+          className="return-link"
+          as="/#articles"
+          title="Go back to home page"
+        >
+          <FontAwesomeIcon icon={faArrowLeft} className="link-icon" />
+          Go back
+        </Link>
+        <header className="akata-article-header w-100">
+          <div className="header-title d-flex flex-col">
+            <div className="title-text d-flex flex-col">
+              <h1 className="akata-title-strong">
+                Explore the <span> World of Innovation</span>
+              </h1>
+              <p className="content akata-text-big">
+                Dive into our captivating articles to stay at the forefront of
+                technology and innovation. Explore exciting topics and in-depth
+                analyses that will inspire you to push boundaries. Join us on
+                our quest for knowledge and inspiration
+              </p>
             </div>
-          ))}
-        </div>
-      </header>
-
-      <div className="article-lists">
-        <h2 className="list-title">Explore all articles</h2>
-        <div className="list-card">
-          {ArticleData.map((data, index) => (
-            <ArticleCard
-              key={index}
-              date={data.date}
-              content={data.content}
-              title={data.title}
-              readMore={closeModal}
-              imgSrc={data.imageSrc}
+            <Button
+              ariaLabel="Let's talk about your project requirement"
+              content="LET'S TALK"
+              onClick={openModal}
+              type="button"
+              hoverType="shadowed"
             />
-          ))}
+          </div>
+          <div className="header-article-suggestion d-flex-space-between flex-col">
+            {ArticleData.slice(0, 2).map((data, index) => (
+              <div
+                className="horizontal-article d-flex "
+                key={`horizontal-article-${index}`}
+              >
+                <button className="read-more btn">
+                  <FontAwesomeIcon icon={faChevronRight} className="btn-icon" />
+                </button>
+                <Image
+                  className="article-poster"
+                  alt="article poster"
+                  src={data.imageSrc}
+                  width={220}
+                  height={166}
+                />
+                <div className="article-text-content d-flex flex-col">
+                  <span className="date">{data.date}</span>
+                  <h3 className="title">{data.title}</h3>
+                  <p className="content akata-text-small">
+                    {truncate(data.content, 127)}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </header>
+
+        <div className="article-lists">
+          <h2 className="list-title">Explore all articles</h2>
+          <div className="list-card">
+            {ArticleData.map((data, index) => (
+              <ArticleCard
+                key={index}
+                date={data.date}
+                content={data.content}
+                title={data.title}
+                readMore={closeModal}
+                imgSrc={data.imageSrc}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
