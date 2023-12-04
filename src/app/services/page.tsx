5d01a9b884377917c720c0217eb802675ca3a75b
nalahatra-akata <nalahatra-akata@goavana.com>
2023-12-04 11:41:45 +0300

    feat: custom button ui

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,7 @@
 import dynamic from "next/dynamic";
 import "./services-page.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 const Services = dynamic(
   () => import("@/components/sections/services/Services")
 );
@@ -7,7 +9,9 @@ export default function Page() {
   return (
     <div className="services-page">
       <div className="services-page-return-content container">
-        <button className="return-button">go back</button>
+        <button className="return-button" type="button">
+          <FontAwesomeIcon icon={faArrowLeft} className="icon-btn" /> go back
+        </button>
       </div>
       <Services />
     </div>
